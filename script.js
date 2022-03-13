8ea9a5b85ea0966b3ccc6c3da4703f86ccea4851
MOSES-I <masmos702@gmail.com>
2022-03-13 13:02:29 +0530

    The Calculator for Everyone Can Get Rich

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-function compute()
+function compute()        <!--For interest calculation and displaying it-->
 {
-    p = document.getElementById("principal").value;
-    
+var principal = document.getElementById("principal").value;
+if (principal<=0)                        <!-- For alerting, if entered negative and 0 values--> 
+{
+alert("Enter a positive number");
+var check = document.getElementById("principal");
+check.focus();
+return false;
 }
-        
+
+var rate = document.getElementById("rate").value;
+var years = document.getElementById("years").value;
+var interest = principal * years * rate / 100 ;
+var year = new Date().getFullYear()+parseInt(years); 
+document.getElementById("result").innerText="If you deposit "+principal+", \n at an interest rate of "+rate+"%. \n You will receive an amount of "+interest+", \n in the year "+year;
+}
+
+function updateRate()              <!-- To display the value of slider -->
+{
+var rateval = document.getElementById("rate").value;
+document.getElementById("rate_val").innerText=rateval;
+}
